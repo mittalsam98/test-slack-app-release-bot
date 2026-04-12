d4d23826891ee786896258c670745f16abaeee43
Sachin Mittal <42431274+mittalsam98@users.noreply.github.com>
2026-04-12 20:54:18 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ console.log('hdllde');
 
 console.log('added secomd console');
 
-//edit my file do
+//edit my file do 1
 
 console.log('added more console after neon db minor update s');
 // what is this bro
