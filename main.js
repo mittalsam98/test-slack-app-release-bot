8251b28221895820c5062480071fe2e4c30f2c15
Sachin Mittal <42431274+mittalsam98@users.noreply.github.com>
2026-04-13 00:33:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-console.log('hdllde');
-
-console.log('added secomd console');
-
-
-
-console.log('added more console after neon db minor update');
-
-// what is this bro
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    
+};
